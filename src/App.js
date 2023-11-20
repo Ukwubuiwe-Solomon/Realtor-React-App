333e791494f23b93ae25837a83c1eacc0ac66a7a
bobojete <uku.ceo@gmail.com>
2023-11-20 17:18:25 +0100

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Offers from './pages/Offers';
+import ForgotPassword from './pages/ForgotPassword';
 
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-red-700'>hello. world</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
