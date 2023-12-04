e9af35ff4ef3c5403cdfe6d0089302b29bb81b5c
bobojete <uku.ceo@gmail.com>
2023-12-04 16:39:40 +0100

    complete forgot password functionality

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import {Link} from "react-router-dom"
 import OAuth from "../components/OAuth";
+import { toast } from "react-toastify";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 
 const ForgotPassword = () => {
   
@@ -9,6 +11,16 @@ const ForgotPassword = () => {
 
   function onChange(e) {
     setEmail(e.target.value);
+  }
+  async function onSubmit(e){
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      await sendPasswordResetEmail(auth, email)
+      toast.success("Email was sent")
+    } catch (error) {
+      toast.error("Could not send resent password")
+    }
   }
   return (
     <section>
@@ -22,7 +34,7 @@ const ForgotPassword = () => {
           />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-6"
               type="email"
